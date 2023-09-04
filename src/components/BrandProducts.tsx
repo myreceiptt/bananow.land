79f804594f0f7c8ffa3ac1a6db4e02beb11a5ccb
myreceiptt <nota@straight-line.org>
2023-09-05 01:50:30 +0700

    Now Class

--- src/components/BrandProducts.tsx
+++ src/components/BrandProducts.tsx
@@ -110,7 +110,7 @@ export function BrandProducts() {
               />
             </Link>
           </div>
-          <h2 className="font-bold text-lg text-center text-neutral-900 dark:text-white"><span className="text-green-now dark:text-yellow-now font-bold">BANANOW</span> Class</h2>
+          <h2 className="font-bold text-lg text-center text-neutral-900 dark:text-white">Now Class</h2>
           <p className="pt-2 text-base text-center text-dark-now dark:text-white-now">
             An intimate education approach for the community. The room to share about love, life, and technology. It can be on a Discord channel or in any physical room in the Universe of Reality.
           </p>
