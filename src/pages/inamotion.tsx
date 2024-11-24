a85d8556f94b9c298dbf8f51fe5d6bfc0b577a31
Prof. NOTA <141787406+myreceiptt@users.noreply.github.com>
2024-11-24 23:53:00 +0700

    Update inamotion.tsx

--- src/pages/inamotion.tsx
+++ src/pages/inamotion.tsx
@@ -16,7 +16,7 @@ export default function InaMotion() {
       <Head>
         <meta
           httpEquiv="Refresh"
-          content="4; url='https://gp.bananow.land/the-ecosystem/the-brands/inamotion'"
+          content="4; url='https://bananowgp.endhonesa.com/the-ecosystem/the-brands/inamotion'"
         />
       </Head>
       <div
@@ -47,7 +47,7 @@ export default function InaMotion() {
           </p>
           <Link
             onClick={sound2Click}
-            href="https://gp.bananow.land/the-ecosystem/the-brands/inamotion"
+            href="https://bananowgp.endhonesa.com/the-ecosystem/the-brands/inamotion"
           >
             <button className="bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 text-base rounded-full px-16 p-3 font-medium dark:hover:bg-yellow-now hover:bg-green-now hover:text-neutral-900 dark:hover:text-white">
               InAMotion on The Green Print
