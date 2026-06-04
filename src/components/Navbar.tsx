f56190e3f121d3dcd8713954d0f1bdc04f9a027f
myreceiptt <nota@endhonesa.com>
2026-06-04 18:09:18 +0700

    oldbrand layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,46 +25,31 @@ function classNames(...classes: any) {
 
 export default function Navbar() {
   const sound0Loc = "/sounds/tiuing.mp3";
-  const [play0] = useSound(
-    sound0Loc,
-    { volume: 0.75 }
-  );
+  const [play0] = useSound(sound0Loc, { volume: 0.75 });
   const sound0Click = () => {
     play0();
   };
 
   const sound1Loc = "/sounds/nguing.mp3";
-  const [play1] = useSound(
-    sound1Loc,
-    { volume: 0.75 }
-  );
+  const [play1] = useSound(sound1Loc, { volume: 0.75 });
   const sound1Click = () => {
     play1();
   };
 
   const sound2Loc = "/sounds/tinggg.mp3";
-  const [play2] = useSound(
-    sound2Loc,
-    { volume: 0.75 }
-  );
+  const [play2] = useSound(sound2Loc, { volume: 0.75 });
   const sound2Click = () => {
     play2();
   };
 
   const sound4Loc = "/sounds/ngung-ngung.mp3";
-  const [play4, { stop }] = useSound(
-    sound4Loc,
-    { volume: 1.25 }
-  );
-  const [isHovering, setIsHovering] = React.useState(
-    false
-  );
+  const [play4, { stop }] = useSound(sound4Loc, { volume: 1.25 });
+  const [isHovering, setIsHovering] = React.useState(false);
 
   return (
     <Disclosure
       as="nav"
-      className="fixed top-0 left-0 right-0 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-400 border-b border-neutral-200 dark:border-neutral-700 backdrop-blur-sm bg-white/90 dark:bg-neutral-900/80 z-20"
-    >
+      className="fixed top-0 left-0 right-0 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-400 border-b border-neutral-200 dark:border-neutral-700 backdrop-blur-sm bg-white/90 dark:bg-neutral-900/80 z-20">
       {({ open }: { open: any }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -103,10 +88,9 @@ export default function Navbar() {
                           item.current
                             ? "text-neutral-900 dark:text-white "
                             : "text-dark-now dark:text-white-now hover:underline hover:text-neutral-900 dark:hover:text-white",
-                          "xl:text-base xl:font-medium text-sm font-normal"
+                          "xl:text-base xl:font-medium text-sm font-normal",
                         )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
+                        aria-current={item.current ? "page" : undefined}>
                         {item.name}
                       </Link>
                     ))}
@@ -118,11 +102,8 @@ export default function Navbar() {
                     onClick={sound2Click}
                     href="/nfts/"
                     className="hidden lg:block"
-                    title="Be a Head of BANANOW.LAND..."
-                  >
-                    <button
-                      className="bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-2 xl:text-base xl:font-medium text-sm font-normal dark:hover:bg-yellow-now hover:bg-yellow-now hover:text-neutral-900 dark:hover:text-white rounded-full"
-                    >
+                    title="Be a Head of BANANOW.LAND...">
+                    <button className="bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-2 xl:text-base xl:font-medium text-sm font-normal dark:hover:bg-yellow-now hover:bg-yellow-now hover:text-neutral-900 dark:hover:text-white rounded-full">
                       Be a Head
                     </button>
                   </Link>
@@ -131,11 +112,8 @@ export default function Navbar() {
                     href="https://bananowgp.endhonesa.com/"
                     target="_blank"
                     className="hidden lg:block"
-                    title="Read The Green Print of BANANOW.LAND..."
-                  >
-                    <button
-                      className="bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-2 xl:text-base xl:font-medium text-sm font-normal dark:hover:bg-green-now hover:bg-green-now hover:text-neutral-900 dark:hover:text-white rounded-full"
-                    >
+                    title="Read The Green Print of BANANOW.LAND...">
+                    <button className="bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-2 xl:text-base xl:font-medium text-sm font-normal dark:hover:bg-green-now hover:bg-green-now hover:text-neutral-900 dark:hover:text-white rounded-full">
                       The Green Print
                     </button>
                   </Link>
@@ -144,7 +122,9 @@ export default function Navbar() {
 
                 <div className="absolute inset-y-0 right-0 flex items-center lg:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button onClick={sound0Click} className="inline-flex items-center justify-center rounded-md text-dark-now dark:text-light-now ">
+                  <Disclosure.Button
+                    onClick={sound0Click}
+                    className="inline-flex items-center justify-center rounded-md text-dark-now dark:text-light-now ">
                     <span className="sr-only">Open The Menu</span>
                     {open ? (
                       <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -169,23 +149,25 @@ export default function Navbar() {
                     item.current
                       ? "text-neutral-900 dark:text-white"
                       : "text-dark-now dark:text-white-now",
-                    "block py-4 text-base font-medium border-b border-neutral-200 dark:border-neutral-700"
+                    "block py-4 text-base font-medium border-b border-neutral-200 dark:border-neutral-700",
                   )}
-                  aria-current={item.current ? "page" : undefined}
-                >
+                  aria-current={item.current ? "page" : undefined}>
                   {item.name}
                 </Disclosure.Button>
               ))}
               <Link
                 onClick={sound2Click}
-                href="/nfts/" title="Be a Head of BANANOW.LAND...">
+                href="/nfts/"
+                title="Be a Head of BANANOW.LAND...">
                 <button className="mt-12 bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-3 font-medium rounded-full w-full">
                   Be a Head
                 </button>
               </Link>
               <Link
                 onClick={sound2Click}
-                href="https://bananowgp.endhonesa.com/" target="_blank" title="Read The Green Print of BANANOW.LAND...">
+                href="https://bananowgp.endhonesa.com/"
+                target="_blank"
+                title="Read The Green Print of BANANOW.LAND...">
                 <button className="mt-12 bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 px-4 p-3 font-medium rounded-full w-full">
                   The Green Print
                 </button>
--- src/pages/oldbrands.tsx
+++ src/pages/oldbrands.tsx
@@ -1,44 +1,58 @@
+import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
 import useSound from "use-sound";
+import React from "react";
 
 const oldBrands = [
   {
-    name: "90’s Vibes",
+    name: "90's Vibes",
     url: "/vibes",
     description:
       "The space (mostly on X - Twitter and Discord) where we can sing, chat, share, shill, or laugh with the fun of 90's spirit. Speak up your voices, make a lot of noise, and don't be silent!",
+    title: "The 90's Vibes Space",
+    images: "/images/brandproducts/90svibes.svg",
   },
   {
     name: "Dermaga Web3",
     url: "/dermaga",
     description:
       "The offline event that shares and exchanges knowledge about the Web3 world. The bridge between Web2 and Web3 community.",
+    title: "Dermaga NFT is Part of Dermaga Web3",
+    images: "/images/brandproducts/dermaga.svg",
   },
   {
     name: "Class Now",
     url: "/class",
     description:
       "An intimate education approach for the community. The room to share about love, life, and technology. It can be on a Discord channel or in any physical room in the Universe of Reality.",
+    title: "Discord Class is Part of BANANOW Class",
+    images: "/images/brandproducts/class.svg",
   },
   {
     name: "InAMotion",
     url: "/inamotion",
     description:
       "The creative agency that accommodates the creative needs of other brands/products/entities, and is a profitable employer for creative people in the BANANOW community.",
+    title: "Ina Motion Creative Agency",
+    images: "/images/brandproducts/inamotion.svg",
   },
   {
     name: "Agent Now",
     url: "/delegate",
     description:
       "The talent coordinator agency of BANANOW which is delegates the demand from other entities to farmers in BANANOW FAMILY.",
+    title: "Agent Now is Farmers Delegate",
+    images: "/images/brandproducts/delegate.svg",
   },
   {
-    name: "NOTA’s Dept.",
+    name: "NOTA's Dept.",
     url: "/professor",
     description:
       "The Web3 tech support department which makes the engine run smoothly in BANANOW. Doing tech strategy, Web3 philosophical research, blockchain exploration, and even soul analytics.",
+    title: "Prof. NOTA's Department",
+    images: "/images/brandproducts/professornota.svg",
   },
 ];
 
@@ -49,60 +63,77 @@ export default function OldBrands() {
     play2();
   };
 
+  const sound4Loc = "/sounds/ngung-ngung.mp3";
+  const [play4, { stop }] = useSound(sound4Loc, { volume: 1.25 });
+  const [isHovering, setIsHovering] = React.useState(false);
+
   return (
     <Layout>
       <div
         className="bg-white dark:bg-neutral-900 w-full px-4 pt-32 pb-16"
-        id="oldbrands"
-      >
+        id="oldbrands">
         <h2 className="text-4xl font-judul font-bold text-center text-neutral-900 dark:text-white">
-          Here are BANANOW&apos;s Old Brands
+          Here are{" "}
+          <span className="text-green-now dark:text-yellow-now font-extrabold">
+            BANANOW
+          </span>
+          's Old Brands
         </h2>
 
-        <div className="pt-6 pb-16 text-base max-w-4xl text-left m-auto text-dark-now dark:text-white-now">
-          <p>
-            Our{" "}
-            <span className="text-green-now dark:text-yellow-now font-bold">
-              FAMILY
-            </span>{" "}
-            is actually trying the best to always maintain the fertility of each
-            of the plantation crops on this{" "}
-            <span className="text-yellow-now dark:text-green-now font-bold">
-              LAND
-            </span>
-            . Make this space a healthy, fair, and fun place to grow.
-          </p>
-          <p className="pt-6">
-            But time goes by. What is planted grows. What is born also grows to
-            maturity. What is young grows old. What is old eventually dies and
-            fertilizes the soil, making everything planted can grow.
-          </p>
-          <p className="pt-6">
-            So, if you want to know more about what we do in the early times,
-            click on our old brands below.
-          </p>
-        </div>
+        <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto text-dark-now dark:text-white-now">
+          Our{" "}
+          <span className="text-green-now dark:text-yellow-now font-bold">
+            FAMILY
+          </span>{" "}
+          is actually trying the best to always maintain the fertility of each
+          of the plantation crops on this{" "}
+          <span className="text-yellow-now dark:text-green-now font-bold">
+            LAND
+          </span>
+          . Make this space a healthy, fair, and fun place to grow.
+        </p>
+        <p className="pb-6 text-base max-w-2xl text-center m-auto text-dark-now dark:text-white-now">
+          But time goes by. What is planted grows. What is born also grows to
+          maturity. What is young grows old. What is old eventually fertilizes
+          the soil, making everything planted can grow. If you want to know more
+          about what we do in the early times, click on our old brands below.
+        </p>
 
-        <div className="max-w-4xl mx-auto grid gap-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-3 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
           {oldBrands.map((brand) => (
-            <div
-              key={brand.name}
-              className="border border-light-now dark:border-light-now rounded-lg p-6"
-            >
-              <h3 className="font-judul font-bold text-2xl text-neutral-900 dark:text-white">
+            <div key={brand.name}>
+              <div className="h-32 w-32 flex justify-center mx-auto hover:scale-110 transition">
+                <Link
+                  onClick={sound2Click}
+                  href={brand.url}
+                  title={brand.title}
+                  target="_blank">
+                  <Image
+                    onMouseEnter={() => {
+                      setIsHovering(true);
+                      play4();
+                    }}
+                    onMouseLeave={() => {
+                      setIsHovering(false);
+                      stop();
+                    }}
+                    src={brand.images}
+                    alt={brand.title}
+                    width={130}
+                    height={130}
+                    quality={75}
+                    sizes="100vw"
+                    priority
+                  />
+                </Link>
+              </div>
+
+              <h2 className="font-judul font-bold text-lg text-center text-neutral-900 dark:text-white">
                 {brand.name}
-              </h3>
-              <p className="pt-4 text-base text-dark-now dark:text-white-now">
+              </h2>
+              <p className="pt-2 text-base text-center text-dark-now dark:text-white-now">
                 {brand.description}
               </p>
-              <Link
-                onClick={sound2Click}
-                href={brand.url}
-                target="_blank"
-                className="mt-6 inline-block bg-dark-now dark:bg-white-now text-white dark:text-neutral-900 text-base rounded-full px-8 p-3 font-medium dark:hover:bg-yellow-now hover:bg-green-now hover:text-neutral-900 dark:hover:text-white"
-              >
-                Visit {brand.name}
-              </Link>
             </div>
           ))}
         </div>
