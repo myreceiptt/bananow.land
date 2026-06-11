e2f9dc67105900d09411eb9db8b7a257ba9514c9
myreceiptt <nota@endhonesa.com>
2026-06-11 18:13:49 +0700

    reorganize page

--- src/pages/nfts/index.tsx
+++ src/pages/nfts/index.tsx
@@ -1,4 +1,4 @@
-import PlaceholderPage from "../components/ui/PlaceholderPage";
+import PlaceholderPage from "../../components/ui/PlaceholderPage";
 
 export default function Nfts() {
   return (
---
+++ src/pages/skool/index.tsx
@@ -0,0 +1,24 @@
+import Embassy from "../../components/ui/Embassy";
+import Layout from "../../components/ui/Layout";
+import { SkoolFaq } from "../../components/skool/SkoolFaq";
+import { SkoolHero } from "../../components/skool/SkoolHero";
+import { SkoolHowItWorks } from "../../components/skool/SkoolHowItWorks";
+import { SkoolPartners } from "../../components/skool/SkoolPartners";
+import { SkoolPaths } from "../../components/skool/SkoolPaths";
+import { SkoolPrograms } from "../../components/skool/SkoolPrograms";
+
+export default function Skool() {
+  return (
+    <Layout
+      title="Skool Now - BANANOW Land"
+      description="Skool Now is BANANOW Land's home for learning programs, sports communities, real-world skills, partners, sponsors, and future literacy.">
+      <SkoolHero />
+      <SkoolPaths />
+      <SkoolHowItWorks />
+      <SkoolPrograms />
+      <SkoolPartners />
+      <SkoolFaq />
+      <Embassy />
+    </Layout>
+  );
+}
